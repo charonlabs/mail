1a4752ba761818882e07baa6e657efbe284a0e69
wsfhahn <wsfhahn@icloud.com>
2025-08-21 22:40:28 -0400

    build_swarm_from_json_str implementation

--- src/mail/swarms/builder.py
+++ src/mail/swarms/builder.py
@@ -37,3 +37,30 @@ def build_swarm_from_name(name: str) -> Swarm:
             return Swarm(name=name, agents=swarm_agents)
 
     raise ValueError(f"swarm '{name}' not found in swarms.json")
+
+def build_swarm_from_json_str(json_swarm: str, name: str) -> Swarm:
+    try:
+        swarms = json.loads(json_swarm)
+
+        swarm_agents: list[Agent] = []
+        for swarm in swarms:
+            if swarm["name"] == name:
+                for agent in swarm["agents"]:
+                    swarm_agents.append(
+                        Agent(
+                            name=agent["name"],
+                            factory=read_python_string(agent["factory"]),
+                            llm=agent["llm"],
+                            system=read_python_string(agent["system"]),
+                            comm_targets=agent["comm_targets"],
+                            agent_params=agent["agent_params"]
+                        )
+                    )
+                
+                return Swarm(name=name, agents=swarm_agents)
+        raise ValueError(f"Swarm {name} not found in swarms JSON!")
+    except Exception as e:
+        raise Exception(f"Failed to build swarm from JSON string: {e}") from e
+
+if __name__ == "__main__":
+    json.loads
