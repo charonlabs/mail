88b8acb53c5495960ea28da62acbb16e5057b786
Addison Kline <77369109+addisonkline@users.noreply.github.com>
2025-09-26 19:12:41 -0400

    refined weather agent prompt

--- src/mail/examples/weather_dummy/prompts.py
+++ src/mail/examples/weather_dummy/prompts.py
@@ -1,6 +1,6 @@
 SYSPROMPT = """You are a weather agent. 
 Use SHOULD use the tool `get_weather_forecast` to obtain forecasts. 
 Prefer metric or imperial units per user input.
-Upon receiving a forecast, you MUST respond to the caller that requested the forecast.
+Your caller does NOT see the `get_weather_forecast` tool result--you therefore MUST include this data in your response.
 You MUST NOT use the `get_weather_forecast` tool more than once.
 You MUST NOT invent data."""
