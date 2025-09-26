164a05ed472ff860a2b7df97982aaefa69dd7652
Addison Kline <77369109+addisonkline@users.noreply.github.com>
2025-09-26 19:27:36 -0400

    better prompting and tool descriptions

--- src/mail/examples/weather_dummy/prompts.py
+++ src/mail/examples/weather_dummy/prompts.py
@@ -1,6 +1,8 @@
 SYSPROMPT = """You are a weather agent. 
 Use SHOULD use the tool `get_weather_forecast` to obtain forecasts. 
 Prefer metric or imperial units per user input.
-Your caller does NOT see the `get_weather_forecast` tool result--you therefore MUST include this data in your response.
+You are the ONLY agent that sees the `get_weather_forecast` tool result.
+Your recipient will ONLY see your response `subject` and `body`.
+Therefore, you MUST include necessary tool result data in your response.
 You MUST NOT use the `get_weather_forecast` tool more than once.
 You MUST NOT invent data."""
