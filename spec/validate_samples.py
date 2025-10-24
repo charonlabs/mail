17d378f2c10750b221ef7903b4fe0f6ca0d4d545
Addison Kline <77369109+addisonkline@users.noreply.github.com>
2025-10-24 15:45:39 -0400

    updated spec

--- spec/validate_samples.py
+++ spec/validate_samples.py
@@ -103,6 +103,10 @@ def sample_interswarm_request_wrapper():
         "timestamp": iso_now(),
         "payload": payload,
         "msg_type": "request",
+        "task_owner": "user:123@swarm-a",
+        "task_contributors": [
+            "user:123@swarm-a"
+        ],
         "metadata": {"expect_response": True},
     }
 
@@ -209,6 +213,11 @@ def sample_interswarm_response_wrapper():
         "timestamp": iso_now(),
         "payload": payload,
         "msg_type": "response",
+        "task_owner": "user:123@swarm-a",
+        "task_contributors": [
+            "user:123@swarm-a",
+            "swarm:swarm-a@swarm-b"
+        ],
         "metadata": {"expect_response": False},
     }
 
